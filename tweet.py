7321e8c0e3b0939bf904d79c7810eb0d322c030e
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-07-10 19:36:37 -0400

    Created a narcicistic bot that has a search_string that likes two tweets from a user that discusses about python. You can change the search_string and numberOfTweets to anything you want your bot to like on your twitter account.

--- tweet.py
+++ tweet.py
@@ -1,8 +1,9 @@
 import tweepy
 import time
-#Verify the account 
-auth = tweepy.OAuthHandler('xLngEgKUNT54D8whMGbv8X64Z','8Bp4bTD4Cx05oNILgMB5APjfbLyfjUVoEPmjJPFn1aV4usG0q3')
-auth.set_access_token('1281592690098962433-HJ5ZhXm8qmEXBOC7oaWLbUxS4SpKBe', 'zegXS73paQMMHF7r3pp61TV14zcMwfVHrVwE0XwG65WRL')
+#Verify the account by putting in your api key, and secret api key
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#Verify the access token and secret access token. 
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 user = api.me()
 
@@ -14,7 +15,20 @@ def limit_handle(cursor):
         time.sleep(3000)
 #Generous Bot 
 for follower in limit_handle(tweepy.Cursor(api.followers).items()):
-    if follower.name == 'Ajeasmith':
-        follower.follow()
-        break
-  
+    if follower.followers_count > 100:
+       follower.follow()
+       break
+
+
+
+# Narcisistic
+search_string = 'python'
+numbersOfTweets = 2
+for tweet in limit_handle(tweepy.Cursor(api.search, search_string).items(numbersOfTweets)):
+    try:
+        tweet.favorite()
+        print('I liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+            break
