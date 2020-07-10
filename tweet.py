d31de560c9fb27ca91a5d1c7b47a2ef82bc07dad
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-07-10 19:08:52 -0400

    create a limit handle to make sure to not overload the api request, and created a generous bot to follow someone that has more than 100 followers.

--- tweet.py
+++ tweet.py
@@ -1,9 +1,20 @@
 import tweepy
+import time
 #Verify the account 
 auth = tweepy.OAuthHandler('xLngEgKUNT54D8whMGbv8X64Z','8Bp4bTD4Cx05oNILgMB5APjfbLyfjUVoEPmjJPFn1aV4usG0q3')
 auth.set_access_token('1281592690098962433-HJ5ZhXm8qmEXBOC7oaWLbUxS4SpKBe', 'zegXS73paQMMHF7r3pp61TV14zcMwfVHrVwE0XwG65WRL')
-
 api = tweepy.API(auth)
 user = api.me()
 
-print(user.followers_count)
+def limit_handle(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(3000)
+#Generous Bot 
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+    if follower.name == 'Ajeasmith':
+        follower.follow()
+        break
+  
